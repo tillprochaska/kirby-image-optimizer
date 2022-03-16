80e94c6e640866d406838acb7a55195acd04b347
Till Prochaska <mail@tillprochaska.de>
2022-03-16 17:20:33 +0100

    Autoload index.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace TillProchaska\KirbyImageOptimizer\Tests;
 
-use Kirby\Cms\App;
 use Kirby\Filesystem\Dir;
 use TillProchaska\KirbyTestUtils\TestCase as BaseTestCase;
 
@@ -15,10 +14,6 @@ class TestCase extends BaseTestCase
     protected function beforeKirbyInit(): void
     {
         Dir::remove(__DIR__.'/support/kirby/media');
-
-        if (null === App::plugin('tillprochaska/image-optimizer')) {
-            App::plugin('tillprochaska/image-optimizer', require __DIR__.'/../plugin/extensions.php');
-        }
     }
 
     protected function kirbyProps(): array
